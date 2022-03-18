a4a88d7c5f61f8393026858e8ce0c3f72b50f0d7
denkiril <denkiril@gmail.com>
2022-03-19 02:19:48 +0300

    Test env css

--- src/app/components/main-map/main-map.component.ts
+++ src/app/components/main-map/main-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵmarkDirty as markDirty } from '@angular/core';
 
 @Component({
   selector: 'exogb-main-map',
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-map.component.scss'],
 })
 export class MainMapComponent implements OnInit {
+  winW = 0;
+  winH = 0;
+  docW = 0;
+  docH = 0;
+  vw = 0;
+  vh = 0;
+
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method, @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.calcParams();
+
+    setTimeout(() => this.calcParams(), 10000);
+  }
+
+  calcParams(): void {
+    const boxEl = document.querySelector('#box10');
+
+    this.winW = window.innerWidth;
+    this.winH = window.innerHeight;
+    this.docW = document.documentElement.clientWidth;
+    this.docH = document.documentElement.clientHeight;
+    if (boxEl) {
+      this.vw = Math.floor(boxEl.clientWidth * 2.5);
+      this.vh = Math.floor(boxEl.clientHeight * 2.5);
+    }
+
+    markDirty(this);
+  }
 }
